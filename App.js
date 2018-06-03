a4982e183c838d59b7e62f92e75207e88fdf0c85
Adam <trzcinski.adam@gmail.com>
2018-06-03 23:55:35 +0200

    Cache some images on App load

--- App.js
+++ App.js
@@ -2,7 +2,7 @@
 import React from 'react';
 import { DefaultTheme, Provider, Text, Button } from 'react-native-paper';
 import { createDrawerNavigator, createStackNavigator } from 'react-navigation';
-import { Font, AppLoading } from 'expo';
+import { Font, AppLoading, Asset } from 'expo';
 
 import Main from './src';
 import New from './src/New';
@@ -48,7 +48,6 @@ const Router = createStackNavigator(
     },
   },
   {
-    initialRouteName: 'New',
     navigationOptions: ({ navigation }) => ({
       headerLeft: (
         <Button compact icon="menu" flat onPress={navigation.toggleDrawer} />
@@ -77,19 +76,34 @@ export default class Abyss extends React.Component<void, State> {
     fontsLoaded: false,
   };
 
+  loadAssets = async () => {
+    await this.loadImages();
+    await this.loadFonts();
+  };
+
+  loadImages = async () => {
+    const images = [
+      require('./assets/images/logo-abyss.png'),
+      require('./assets/images/background-artbook.jpg'),
+    ];
+    const cacheImages = images.map(image =>
+      Asset.fromModule(image).downloadAsync()
+    );
+    return Promise.all(cacheImages);
+  };
+
   loadFonts = async () => {
     await Font.loadAsync({
       'lato-light': require('./assets/fonts/Lato-Light.ttf'),
       'lato-regular': require('./assets/fonts/Lato-Regular.ttf'),
       spqr: require('./assets/fonts/spqr.ttf'),
     });
-    this.setState({ fontsLoaded: true });
   };
 
   render() {
     return !this.state.fontsLoaded ? (
       <AppLoading
-        startAsync={this.loadFonts}
+        startAsync={this.loadAssets}
         onFinish={() => this.setState({ fontsLoaded: true })}
       />
     ) : (
